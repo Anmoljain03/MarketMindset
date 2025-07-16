71fbfda1ad20470729168af96a71c3024d172f70
Anmol Jain <128278782+Anmoljain03@users.noreply.github.com>
2025-07-17 00:25:52 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,17 +4,22 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), 
-    tailwindcss(), ],
-    server: {
+  plugins: [react(), tailwindcss()],
+  server: {
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
         changeOrigin: true,
-        '/uploads': 'http://localhost:5000', 
+      },
+      '/uploads': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
       },
     },
   },
-
- 
+  preview: {
+    host: true, // Bind to 0.0.0.0
+    port: process.env.PORT || 4173,
+    allowedHosts: ['marketmindset.onrender.com'],
+  },
 })
